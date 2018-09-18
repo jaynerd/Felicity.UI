e519da13dd937f96813b74f647825c2bf4518777
jaynerd <dev.jaynerd@gmail.com>
2018-09-19 11:03:31 +1200

    login screen updated

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,17 @@
 <template>
     <div id='main' class='vm vbox'>
         <app-header/>
-        <section class='vm hbox flex'>
+        <section v-if="authenticated" class='vm hbox flex'>
             <app-nav/>
             <app-board class='flex flex-1'/>
         </section>
+        <app-login v-else/>
         <app-footer/>
     </div>
 </template>
 
 <script>
+import Login from "./auth/Login.vue";
 import Header from "./layout/Header.vue";
 import Nav from "./layout/Nav.vue";
 import Board from "./layout/Board.vue";
@@ -18,10 +20,16 @@ import Footer from "./layout/Footer.vue";
 export default {
   name: "Main",
   components: {
+    "app-login": Login,
     "app-header": Header,
     "app-nav": Nav,
     "app-board": Board,
     "app-footer": Footer
+  },
+  data() {
+    return {
+      authenticated: false
+    };
   },
   mounted() {
     this.$bus.$on("toggle-nav", $event => {
---
+++ src/components/auth/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="login" class="vm cc vbox">
+        <div id="login-box" class="sc vbox">
+            <h3 class="sc flex-1">Please Login</h3>
+            <div id="input-area" class="sc flex-3">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scripts/css/styles.scss";
+
+#login {
+  background-color: red;
+
+  #login-box {
+    width: 60%;
+    height: 80%;
+    background-color: blue;
+
+    h3 {
+      margin: 0;
+      padding-top: 0.5em;
+      font-size: 3em;
+    }
+
+    #input-area {
+      width: 70%;
+      padding: 2em;
+      margin-bottom: 2em;
+      background-color: green;
+    }
+  }
+}
+</style>
