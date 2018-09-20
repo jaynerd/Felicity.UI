b11ade896de41912737c7be3fa68a4db1fff8466
jaynerd <dev.jaynerd@gmail.com>
2018-09-21 10:40:00 +1200

    test deploy

--- src/components/contents/Dashboard.vue
+++ src/components/contents/Dashboard.vue
@@ -1,11 +1,19 @@
 <template>
-    <div id="dashboard">
+    <div id='dashboard' class="flex">
         <h1>Dashboard Contents</h1>
+        <app-team-box/>
+                <app-team-box/>
+
     </div>
 </template>
 
 <script>
+import TeamBox from "../widgets/TeamBox.vue";
+
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  components: {
+    "app-team-box": TeamBox
+  }
 };
 </script>
---
+++ src/components/widgets/TeamBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="team-box" @click="open()">
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TeamBox",
+  methods: {
+    open() {
+      alert("Clicked");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scripts/css/styles.scss";
+
+#team-box {
+  width: 10em;
+  height: 5em;
+  margin: 1em;
+  background-color: red;
+  cursor: pointer;
+}
+</style>
