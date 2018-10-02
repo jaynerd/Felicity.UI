912c2b77159f11f00b0889911bb8e7a86550bad0
Michelle Extross <michelleanneextross@yahoo.co.nz>
2018-10-03 11:12:51 +1300

    Created a basic UI for the invitation and submission page

---
+++ src/components/widgets/Slider.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1>Happiness Slider</h1>
+<p>How happy are you?</p>
+
+<div class="slidecontainer">
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+  <p>Value: <span id="demo"></span></p>
+</div>
+</template>
+
+<script>
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+</script>
